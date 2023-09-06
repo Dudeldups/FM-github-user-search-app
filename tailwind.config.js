4b3177079feb24261434eed288abc42130a64b13
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-09-06 22:24:20 +0200

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        DEFAULT: "#0079ff",
+        gray: "#4b6a9b",
+        dark: "#1e2a47",
+      },
+      gray: {
+        100: "#fefefe",
+        300: "#f6f8ff",
+        500: "#697c9a",
+        700: "#2b3442",
+        900: "ä141d2f",
+      },
+      error: "#f74646",
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
